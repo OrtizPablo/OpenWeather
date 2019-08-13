766c7ad9083924585be7122549482c5cf64b298e
Pablo Ortiz <pablo@ryaltoapp.com>
2019-08-13 19:16:36 +0100

    Initial Commit

---
+++ OpenWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenWeather
+//
+//  Created by Pablo Rodriguez on 13/08/2019.
+//  Copyright © 2019 Pablo Ortiz Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
