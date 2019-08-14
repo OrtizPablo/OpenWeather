dcd447596f7b34da52b91215a69cc5cf18a514eb
Pablo Ortiz <pablo@ryaltoapp.com>
2019-08-14 14:56:44 +0100

    Extend the struct Weather to conform the protocol Decodable

--- OpenWeather/Model/Weather.swift
+++ OpenWeather/Model/Weather.swift
@@ -28,3 +28,50 @@ struct Weather {
         self.windDirection = windDirection
     }
 }
+
+// MARK: - Decodable
+
+extension Weather: Decodable {
+    
+    private enum CodingKeys: String, CodingKey {
+        case weather
+        case main
+        case wind
+    }
+    
+    private enum WeatherKeys: String, CodingKey {
+        case main
+        case icon
+    }
+    
+    private enum MainKeys: String, CodingKey {
+        case temp
+    }
+    
+    private enum WindKeys: String, CodingKey {
+        case speed
+        case deg
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        var weatherArrayForType = try container.nestedUnkeyedContainer(forKey: .weather)
+        
+        var descrptions: [String] = []
+        var icons: [String] = []
+        while(!weatherArrayForType.isAtEnd) {
+            let weather = try weatherArrayForType.nestedContainer(keyedBy: WeatherKeys.self)
+            descrptions.append(try weather.decode(String.self, forKey: .main))
+            icons.append(try weather.decode(String.self, forKey: .icon))
+        }
+        
+        let mainContainer = try container.nestedContainer(keyedBy: MainKeys.self, forKey: .main)
+        let windContainer = try container.nestedContainer(keyedBy: WindKeys.self, forKey: .wind)
+        
+        let temperature = try mainContainer.decode(Double.self, forKey: .temp)
+        let windSpeed = try windContainer.decode(Double.self, forKey: .speed)
+        let windDirection = try windContainer.decode(Double.self, forKey: .deg)
+        
+        self.init(description: descrptions.first ?? "", iconString: icons.first ?? "", temperature: temperature, windSpeed: windSpeed, windDirection: windDirection)
+    }
+}
